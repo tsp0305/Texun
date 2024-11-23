e5a4ffe465b6edc461ed2422ca4a70757c7636e6
tsp0305 <tspriya0305@gmail.com>
2024-11-23 17:12:00 +0530

    updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -124,9 +124,7 @@ export default function Header() {
         <Navbar.Link active={path === '/about'} as={'div'}>
           <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
-        </Navbar.Link>
+       
       </Navbar.Collapse>
     </Navbar>
   );
